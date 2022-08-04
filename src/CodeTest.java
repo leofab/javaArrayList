9f02f5ac2313e3b0d8859e2016f17f4584c533a0
Leo Fabricio <leofabr3@gmail.com>
2022-08-04 15:18:11 -0300

    adding elements to arraylist

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -3,9 +3,18 @@ import java.util.ArrayList;
 public class CodeTest {
   public static void main(String[] args){
 
-    // ArrayLists of Strings
+    // 1st step - Creating an ArrayLists of Strings
     ArrayList<String> strList = new ArrayList<String>();
 
+    // 2nd step - Adding elements to ArrayList;
+    strList.add("master coding app");
+    strList.add("master coding channel");
+    strList.add("master coding pro app");
 
+    // 3rd step - adding elements in specified position
+    strList.add(2,"Hello");
+
+    // print elements of ArrayList
+    System.out.println(strList);
   }
 }
